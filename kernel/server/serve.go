7382cbafdf480ab0397f4691397c31d657cfd641
Daniel <845765@qq.com>
2026-07-02 23:00:46 +0800

    :art: Automatically perform index fixing during idle time https://github.com/siyuan-note/siyuan/issues/17535
    Signed-off-by: Daniel <845765@qq.com>

--- kernel/server/serve.go
+++ kernel/server/serve.go
@@ -1103,7 +1103,6 @@ func serveWebSocket(ginServer *gin.Engine) {
 
 	util.WebSocketServer.HandleMessage(func(s *melody.Session, msg []byte) {
 		start := time.Now()
-		util.RefreshActivity()
 		logging.LogTracef("request [%s]", shortReqMsg(msg))
 
 		if util.IsAuthSession(s) {
