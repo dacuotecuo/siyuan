d0b5f818630820153be45d8bcbeac33f669f1ef5
Daniel <845765@qq.com>
2026-07-25 09:34:44 +0800

    :sparkles: Add database attribute visibility setting https://github.com/siyuan-note/siyuan/issues/18321

--- app/src/types/config.d.ts
+++ app/src/types/config.d.ts
@@ -525,6 +525,10 @@ declare namespace Config {
          */
         displayNetImgMark: boolean;
         /**
+         * Whether to show database attributes at the top of the document
+         */
+        databaseAttrShow: boolean;
+        /**
          * Default state of database attributes
          * - `0`: Expanded
          * - `1`: Collapsed
