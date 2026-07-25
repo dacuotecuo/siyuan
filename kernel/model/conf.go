93335b6bf4570f6606e337f7cfb9a6abd2f58a78
Daniel <845765@qq.com>
2026-07-25 21:20:49 +0800

    :lock: Hide sensitive paths from configuration responses https://github.com/siyuan-note/siyuan/security/advisories/GHSA-h4v5-crx2-3cv4

--- kernel/model/conf.go
+++ kernel/model/conf.go
@@ -1259,6 +1259,9 @@ func HideConfSecret(c *AppConf) {
 	c.MCPOAuth = ""
 	c.CookieKey = ""
 	c.Api = &conf.API{}
+	if nil != c.Export {
+		c.Export.PandocBin = ""
+	}
 	c.Flashcard = &conf.Flashcard{}
 	c.ServerAddrs = []string{}
 	c.Publish = &conf.Publish{}
@@ -1276,6 +1279,7 @@ func HideConfSecret(c *AppConf) {
 	c.System.ConfDir = ""
 	c.System.DataDir = ""
 	c.System.HomeDir = ""
+	c.System.WorkspaceDir = ""
 	c.System.Name = ""
 	c.System.NetworkProxy = &conf.NetworkProxy{}
 }
