d144366d0aec40ecab5a25aa4e9e00be516882f8
Daniel <845765@qq.com>
2026-07-30 12:18:26 +0800

    :sparkles: Support document tabs on mobile https://github.com/siyuan-note/siyuan/issues/16094

--- app/src/types/config.d.ts
+++ app/src/types/config.d.ts
@@ -817,6 +817,7 @@ declare namespace Config {
          * Whether to close all tabs when starting
          */
         closeTabsOnStart: boolean;
+        tabStartupMode: 0 | 1 | 2;
         /**
          * The storage path of the new document
          */
