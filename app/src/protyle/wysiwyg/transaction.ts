f81300be1b626a20ef1a12ee61938a8a376db102
Daniel <845765@qq.com>
2026-07-30 23:24:34 +0800

    :bug: Restore empty paragraph on conversion undo https://github.com/siyuan-note/siyuan/issues/14744

--- app/src/protyle/wysiwyg/transaction.ts
+++ app/src/protyle/wysiwyg/transaction.ts
@@ -2095,6 +2095,7 @@ export const turnEmptyParagraphsIntoTransaction = (options: {
         oldElement.classList.remove("protyle-wysiwyg--select");
         oldElement.removeAttribute("select-start");
         oldElement.removeAttribute("select-end");
+        const oldHTML = oldElement.outerHTML;
         const editableElement = getContenteditableElement(oldElement);
         editableElement.textContent = getEmptyParagraphTargetMarkdown(options.type);
 
@@ -2115,7 +2116,7 @@ export const turnEmptyParagraphsIntoTransaction = (options: {
         }
         return {
             nodeElement,
-            oldHTML: oldElement.outerHTML,
+            oldHTML,
             newElement,
         };
     });
