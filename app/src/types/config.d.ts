a2046f91f30ba9ae117cd059b5bb93bb11412b20
Daniel <845765@qq.com>
2026-08-13 18:30:54 +0800

    :art: Move full-width task list syntax to block settings https://github.com/siyuan-note/siyuan/issues/18702

--- app/src/types/config.d.ts
+++ app/src/types/config.d.ts
@@ -505,7 +505,7 @@ declare namespace Config {
         /**
          * Whether to enable the full-width task list shortcut
          */
-        inlineFullWidthTaskList: boolean;
+        blockFullWidthTaskList: boolean;
         /**
          * Whether to enable the inline mark
          */
