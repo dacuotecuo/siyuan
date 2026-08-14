ca0af5c2de06ca18014622aa17a37bd02715f7e4
Daniel <845765@qq.com>
2026-08-14 21:27:15 +0800

    :art: Collapse Agent thinking by default https://github.com/siyuan-note/siyuan/issues/10540

--- app/src/layout/dock/agent/AgentChat.ts
+++ app/src/layout/dock/agent/AgentChat.ts
@@ -2647,7 +2647,7 @@ export class AgentChat extends Model {
             existingCard.remove();
         }
 
-        const bodyHTML = '<div class="agent-chat__thinking-body agent-chat__thinking-body--preview">' +
+        const bodyHTML = '<div class="agent-chat__thinking-body">' +
             detailLines +
             "</div>";
 
@@ -2664,7 +2664,7 @@ export class AgentChat extends Model {
             '<svg class="agent-chat__thinking-arrow--contract fn__none"><use xlink:href="#iconContract"></use></svg>' +
             "</span>" +
             '<span class="agent-chat__thinking-text">' + escapeHtml(text) + "</span>" +
-            '<span class="agent-chat__thinking-latest fn__none"></span>' +
+            '<span class="agent-chat__thinking-latest"></span>' +
             "</div>" +
             bodyHTML +
             "</div>";
