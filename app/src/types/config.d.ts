0a0ae70502c455c05a1e1fa383c1f4b9b840f54f
Daniel <845765@qq.com>
2026-08-20 16:11:13 +0800

    :art: Improve configurable asset opening https://github.com/siyuan-note/siyuan/issues/10641

--- app/src/types/config.d.ts
+++ app/src/types/config.d.ts
@@ -518,7 +518,8 @@ declare namespace Config {
         codeBlockMiddleDot: boolean;
     }
 
-    export type TAssetOpenAction = "follow-tab" | "current" | "right" | "new-window" | "app" | "folder";
+    export type TAssetOpenAction = "follow-tab" | "current" | "right" | "bottom" | "background" |
+        "new-window" | "app" | "folder";
 
     export interface IAssetOpen {
         click: TAssetOpenAction;
