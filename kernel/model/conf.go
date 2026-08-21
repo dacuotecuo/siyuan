c78fa2cd2a9034425169fd94b72c59084346c92c
Daniel <845765@qq.com>
2026-08-21 21:01:22 +0800

    :art: Refine the mobile bottom bar experience https://github.com/siyuan-note/siyuan/issues/14542

--- kernel/model/conf.go
+++ kernel/model/conf.go
@@ -396,9 +396,7 @@ func InitConf() {
 	if "" == Conf.Appearance.CodeBlockThemeLight {
 		Conf.Appearance.CodeBlockThemeLight = "github"
 	}
-	if nil == Conf.Appearance.StatusBar {
-		Conf.Appearance.StatusBar = &util.StatusBar{}
-	}
+	Conf.Appearance.StatusBar = util.NormalizeStatusBar(Conf.Appearance.StatusBar, util.IsMobileContainer())
 	util.StatusBarCfg = Conf.Appearance.StatusBar
 	if nil == Conf.Appearance.Notifications {
 		Conf.Appearance.Notifications = util.NewNotifications()
