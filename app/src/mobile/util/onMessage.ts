2e4b473516ccafbea0d2259cfeba65380dcde2e4
Daniel <845765@qq.com>
2026-08-23 21:00:56 +0800

    :art: Refine the mobile bottom bar and status layout https://github.com/siyuan-note/siyuan/issues/18912

--- app/src/mobile/util/onMessage.ts
+++ app/src/mobile/util/onMessage.ts
@@ -43,7 +43,7 @@ export const onMessage = (app: App, data: IWebSocketData) => {
                 } else {
                     clearTimeout(statusTimeout);
                     statusElement.innerHTML = `<div class="fn__flex">${data.data.tasks[0].action}<div class="fn__progress"><div></div></div>`;
-                    statusElement.style.bottom = "var(--mobile-bottom-bar-offset)";
+                    statusElement.style.bottom = "var(--mobile-bottom-bar-safe-area)";
                 }
                 break;
             case "setAppearance":
@@ -179,7 +179,7 @@ export const onMessage = (app: App, data: IWebSocketData) => {
                 }
                 clearTimeout(statusTimeout);
                 statusElement.innerHTML = data.msg;
-                statusElement.style.bottom = "var(--mobile-bottom-bar-offset)";
+                statusElement.style.bottom = "var(--mobile-bottom-bar-safe-area)";
                 statusTimeout = window.setTimeout(() => {
                     statusElement.style.bottom = "";
                 }, 12000);
