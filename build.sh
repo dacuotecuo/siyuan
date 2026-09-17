5ec902fdfc0b075a0fb7b29bac7e7a9935f742f6
liuzhifei <liuzhifei@wps.cn>
2026-09-17 14:30:43 +0800

    修改编译

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
-git switch dev &&git fetch origin && git merge origin/master
+#!/bin/sh
+
+set -e
+
+git switch dev
+git fetch origin
+git merge origin/master
 
 cd kernel
 
@@ -6,7 +12,26 @@ cd kernel
 go build --tags "fts5" -o ../app/kernel/SiYuan-Kernel
 
 cd ../app
-pnpm install && pnpm rebuild
+pnpm install
+pnpm rebuild
 
 pnpm run build
-pnpm run dist
+
+case "$(uname -s):$(uname -m)" in
+  Darwin:arm64)
+    pnpm run dist-darwin-arm64
+    ;;
+  Darwin:x86_64|Darwin:amd64)
+    pnpm run dist-darwin
+    ;;
+  Linux:aarch64|Linux:arm64)
+    pnpm run dist-linux-arm64
+    ;;
+  Linux:x86_64|Linux:amd64)
+    pnpm run dist-linux
+    ;;
+  *)
+    echo "Unsupported build platform: $(uname -s) $(uname -m)" >&2
+    exit 1
+    ;;
+esac
